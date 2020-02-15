8b3e1fd5e53267a5405960f3349c0ecc8ffe1c8f
lazaro-programacion <lazaro0089@gmail.com>
2020-02-15 17:25:15 +0100

    arriba

---
+++ src/main/java/com/Lazaro/Lazaro_servicios/LazaroServiciosApplication.java
@@ -0,0 +1,15 @@
+package com.Lazaro.Lazaro_servicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazaroServiciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazaroServiciosApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/Lazaro/Lazaro_servicios/LazaroServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lazaro.Lazaro_servicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazaroServiciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
